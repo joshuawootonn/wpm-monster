e8553a18ae1035cdd070324216ad5c6b08548c7f
Josh Wootonn <joshuawootonn@gmail.com>
2023-08-28 07:52:17 -0500

    feat: add value based editing

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="flex flex-col h-screen">
+	<nav>
+		<h1 class="font-bold">Welcome to SvelteKit</h1>
+	</nav>
+	<slot />
+	<footer>Footer</footer>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import clsx from 'clsx';
+	console.log('hit');
+	let myInput: HTMLInputElement;
+
+	let value: string = 'Lorem ipsum do';
+	$: valueLetters = value?.split('') ?? [];
+	let letters: string[] =
+		`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae lobortis arcu. Aliquam a
+		pharetra justo. Suspendisse sit amet turpis semper, euismod ante at, eleifend ex. Aenean quis
+		lobortis tellus. Etiam pretium tortor et gravida commodo.`.split('');
+</script>
+
+<div class="w-[700px] mx-auto flex flex-col items-start justify-center flex-grow">
+	<input
+		bind:value
+		type="text"
+		class="peer opacity-0"
+		bind:this={myInput}
+		tabindex="0"
+		id="myInput"
+	/>
+	<!-- svelte-ignore a11y-no-static-element-interactions -->
+	<div
+		class="select-none border-transparent peer-focus:border-slate-300 p-4 border-2 text-xl"
+		on:click={(e) => myInput.focus()}
+		on:keydown={(e) => myInput.focus()}
+	>
+		{#each letters as letter, i}
+			<span
+				class={clsx({
+					'text-cyan-500': letter === valueLetters[i],
+					'text-red-500': letter !== valueLetters[i] && valueLetters[i]
+				})}>{letter}</span
+			>
+		{/each}
+	</div>
+	<!-- <div>{value ?? ''}</div> -->
+</div>
