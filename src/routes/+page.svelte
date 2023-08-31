f583e048a6d880397eeaf22b8202b3fc3cd30f3a
Josh Wootonn <joshuawootonn@gmail.com>
2023-08-31 07:04:46 -0500

    feat: basic typing test done with word states

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,39 +1,106 @@
 <script lang="ts">
-	import clsx from 'clsx';
-	console.log('hit');
 	let myInput: HTMLInputElement;
 
-	let value: string = 'Lorem ipsum do';
-	$: valueLetters = value?.split('') ?? [];
-	let letters: string[] =
-		`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae lobortis arcu. Aliquam a
-		pharetra justo. Suspendisse sit amet turpis semper, euismod ante at, eleifend ex. Aenean quis
-		lobortis tellus. Etiam pretium tortor et gravida commodo.`.split('');
+	type Press = {
+		data: string | null;
+		inputType: 'insertText' | 'deleteContentBackward';
+	};
+
+	let keystrokes: Press[] = [];
+
+	$: position = keystrokes.reduce(
+		(acc, keystroke) => {
+			if (keystroke.inputType === 'insertText' && keystroke.data === ' ') {
+				return [...acc, []];
+			} else if (keystroke.inputType === 'insertText' && keystroke.data) {
+				return [...acc.slice(0, -1), (acc.at(-1) ?? []).concat(keystroke.data)];
+			} else if (keystroke.inputType === 'deleteContentBackward') {
+				if (acc.at(-1)?.length === 0) {
+					return acc.slice(0, -1);
+				}
+
+				return [...acc.slice(0, -1), (acc.at(-1) ?? []).slice(0, -1)];
+			}
+			return [...acc];
+		},
+		[[]] as string[][]
+	);
+
+	function handleInput(e: any) {
+		if (e.inputType === 'insertText' || e.inputType === 'deleteContentBackward')
+			keystrokes = keystrokes.concat({ inputType: e.inputType, data: e.data });
+	}
+
+	function isEqual(a: string[], b: string[]) {
+		return a.length === b.length && a.every((A, i) => A === b.at(i));
+	}
+
+	let words: string[][] = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`
+		.split(' ')
+		.map((word) => word.split(''));
 </script>
 
 <div class="w-[700px] mx-auto flex flex-col items-start justify-center flex-grow">
 	<input
-		bind:value
 		type="text"
 		class="peer opacity-0"
 		bind:this={myInput}
+		on:input={handleInput}
 		tabindex="0"
 		id="myInput"
 	/>
+	{position}
 	<!-- svelte-ignore a11y-no-static-element-interactions -->
 	<div
-		class="select-none border-transparent peer-focus:border-slate-300 p-4 border-2 text-xl"
+		class="flex flex-wrap select-none border-transparent peer-focus:border-slate-300 p-4 border-2 text-xl"
 		on:click={(e) => myInput.focus()}
 		on:keydown={(e) => myInput.focus()}
 	>
-		{#each letters as letter, i}
-			<span
-				class={clsx({
-					'text-cyan-500': letter === valueLetters[i],
-					'text-red-500': letter !== valueLetters[i] && valueLetters[i]
-				})}>{letter}</span
-			>
+		{#each words as word, i}
+			{@const typedWord = position.at(i)}
+			{@const extras = typedWord?.slice(word.length)}
+			{@const isPreviouslyTyped = position.at(i + 1)}
+			<div class:word class:error={typedWord && isPreviouslyTyped && !isEqual(word, typedWord)}>
+				{#if extras?.length && typedWord}
+					{#each typedWord as letter, j}
+						{@const correctLetter = word?.at(j)}
+						<span
+							class:correct={letter === correctLetter}
+							class:incorrect={correctLetter && letter !== correctLetter}
+							class:extra={j > word.length - 1}>{correctLetter ?? letter}</span
+						>
+					{/each}
+				{:else}
+					{#each word as letter, j}
+						{@const typedLetter = typedWord?.at(j)}
+						<span
+							class:correct={letter === typedLetter}
+							class:incorrect={typedLetter && letter !== typedLetter}>{letter}</span
+						>
+					{/each}
+				{/if}
+			</div>
 		{/each}
 	</div>
-	<!-- <div>{value ?? ''}</div> -->
 </div>
+
+<style>
+	.correct {
+		@apply text-green-500;
+	}
+
+	.incorrect {
+		@apply text-red-500;
+	}
+
+	.extra {
+		@apply text-red-800;
+	}
+
+	.word {
+		@apply block m-1;
+	}
+	.error {
+		@apply underline decoration-red-500;
+	}
+</style>
